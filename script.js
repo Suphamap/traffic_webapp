d0ac8932255ea8a2d0950827d77d66c98f9af8a0
Suphanut Mapiam <suphanut.map@gmail.com>
2025-06-07 18:24:16 +0700

    Add HTML structure and styles for traffic prediction visualization pages

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic script file for future interactivity
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Traffic Visualization App Loaded');
+    // Future JavaScript code for map integration, data fetching, etc. will go here.
+});
